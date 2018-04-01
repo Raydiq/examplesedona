14e0f69e7e0709b54cc4777b7159ac33ec920550
Raydiq <36772692+Raydiq@users.noreply.github.com>
2018-04-02 00:27:55 +0300

    закончила исправление ошибок. Вариант для второй проверки.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 var link = document.querySelector(".search-hotel");
 var popup = document.querySelector(".form-frame");
+var arrivalDate = popup.querySelector("[name=date]");
+var adult = popup.querySelector("[name=adult]");
+var child = popup.querySelector("[name=child]");
+var form = document.querySelector("form");
+
+
  link.addEventListener("click", function (evt) {
  	evt.preventDefault(); 
  popup.classList.toggle("form-frame");
  popup.classList.toggle("form-frame-none");  
- }); 
+ });  
+form.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  if (!arrivalDate.value && !adult.value && !child.value) {
+    popup.classList.remove("modal-error");
+    form.offsetWidth = form.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
